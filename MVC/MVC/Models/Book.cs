3f074310978b66e524b52b84096c938b46b12e3b
LokBast <cool.xatiko2012@mail.ru>
2017-12-08 16:32:13 +0300

    start
    Подключение к БД, создание классов "Book" и "User"

---
+++ MVC/MVC/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Book
+    {
+        public string BookId { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public string Status { get; set; }
+    }
+}
