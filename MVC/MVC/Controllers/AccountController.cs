0a404ed183f06a517e925e77c7ba5de6e25238a9
LokBast <cool.xatiko2012@mail.ru>
2017-12-15 15:12:54 +0300

    0.0.8
    Конец :(

--- MVC/MVC/Controllers/AccountController.cs
+++ MVC/MVC/Controllers/AccountController.cs
@@ -3,41 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using MVC.Models;
 
-namespace MVC.Controllers
+namespace LibraryApp.Controllers
+
 {
     public class AccountController : Controller
     {
-        public ActionResult LogOn()
+        public ActionResult Login()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult LogOn(LibraryEntities model, string returnUrl)
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LogOn model)
+        {
+            //LibraryEntities db = new LibraryEntities();
+            if (ModelState.IsValid)
+            {
+                // ищем пользователя в базе данных
+                User user = null;
+                using (OurDbContext db = new OurDbContext())
+                {
+                    user = db.User.FirstOrDefault(u => u.Email == model.Email && u.Password == model.Password);
+
+                }
+                if (user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Email, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователя с таким логином и паролем нет");
+                }
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration(Reg)
         {
             if (ModelState.IsValid)
             {
-                if (true)
+                User user = null;
+                using (LibraryEntities db = new LibraryEntities())
                 {
+                    user = db.User.FirstOrDefault(u => u.Email == db.User);
+                }
+                if (user == null)
+                {
+                  
+                    using (LibraryEntities db = new LibraryEntities())
+                    {
+                        db.userAccount.Add(new User { Email = db.Email, Password = db.Password, Name = db.UserName });
+                        db.SaveChanges();
 
-                    if (Url.IsLocalUrl(returnUrl))
-                    {
-                        return Redirect(returnUrl);
+                        //user = db.userAccount.Where(u => u.Email == db.Email && u.Password == db.Password).FirstOrDefault();
                     }
-                    else
+                    
+                    if (user != null)
                     {
+                        FormsAuthentication.SetAuthCookie(db.Email, true);
                         return RedirectToAction("Index", "Home");
                     }
                 }
                 else
                 {
-                    ModelState.AddModelError("", "The user name or password is incorrect.");
+                    ModelState.AddModelError("", "Пользователь с таким логином уже существует");
                 }
             }
-            return View();
-        }
 
+            return View(model);
+        }
+        public ActionResult Logoff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
--- MVC/MVC/Controllers/HomeController.cs
+++ MVC/MVC/Controllers/HomeController.cs
@@ -4,23 +4,54 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVC.Models;
+using System.Data.Entity;
 
 namespace MVC.Controllers
 {
     public class HomeController : Controller
     {
         LibraryEntities db = new LibraryEntities();
-        public ActionResult Index()
-        {        
+        public ActionResult Index(string bookId, string author)
+        {/*
+            ViewBag.booksId = db.Book;
+            //IQueryable<Book> book = db.Book.Include(p => p.BookId);
+            if (bookId != null && bookId != "" && booksId.BookID == db.Book)
+            {
+                ViewBag.Books = book.Where(p => p.BookId == bookId);            
+            }
+
+            FilterContext fc = new FilterContext();
+            {
+                
+            }*/
+
             ViewBag.Books = db.Book;
             return View();
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         public ActionResult Login()
         {
             return View();
         }
 
+        
         //[Authorize (Users = "user")]
         public ActionResult Reserve()
         {
